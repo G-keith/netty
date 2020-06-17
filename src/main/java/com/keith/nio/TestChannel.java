1ae4bc456b0bf5744aba20f2506c957cb6f9b73c
G-keith <1605763261@qq.com>
2020-06-17 14:41:26 +0800

    nio  channel

---
+++ src/main/java/com/keith/nio/TestChannel.java
@@ -0,0 +1,85 @@
+package com.keith.nio;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-06-17
+ */
+public class TestChannel {
+
+    public static void main(String[] args) throws IOException {
+       //write ();
+        //read();
+        //readWrite();
+        transferFrom();
+    }
+
+    public static void write() throws IOException{
+        String s="hello netty";
+
+        //创建一个输出流
+        FileOutputStream fileOutputStream=new FileOutputStream("/Users/gemi/Desktop/2.txt");
+        //获取 对应的 FileChannel
+        FileChannel fileChannel=fileOutputStream.getChannel();
+
+        //创建一个缓冲区 ByteBuffer
+        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
+        byteBuffer.put(s.getBytes());
+
+        byteBuffer.flip();
+
+        //将 byteBuffer 数据写入到 fileChannel
+        fileChannel.write(byteBuffer);
+        fileOutputStream.close();
+    }
+
+    public static void read() throws IOException{
+
+        File file=new File("/Users/gemi/Desktop/2.txt");
+        FileInputStream fileInputStream=new FileInputStream(file);
+        FileChannel fileChannel=fileInputStream.getChannel();
+
+        ByteBuffer byteBuffer=ByteBuffer.allocate((int) file.length());
+
+        fileChannel.read(byteBuffer);
+        System.out.println(new String(byteBuffer.array()));
+        fileInputStream.close();
+    }
+
+    public static void readWrite() throws IOException{
+        //读
+        File file=new File("/Users/gemi/Desktop/2.txt");
+        FileInputStream fileInputStream=new FileInputStream(file);
+        FileChannel fileChannel=fileInputStream.getChannel();
+        ByteBuffer byteBuffer=ByteBuffer.allocate((int) file.length());
+        fileChannel.read(byteBuffer);
+        //写
+        FileOutputStream fileOutputStream=new FileOutputStream("/Users/gemi/Desktop/3.txt");
+        FileChannel fileChannel2=fileOutputStream.getChannel();
+        byteBuffer.flip();
+        fileChannel2.write(byteBuffer);
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+    public static void transferFrom() throws IOException{
+        //读通道
+        File file=new File("/Users/gemi/Desktop/2.txt");
+        FileInputStream fileInputStream=new FileInputStream(file);
+        FileChannel fileChannel=fileInputStream.getChannel();
+        //写通道
+        FileOutputStream fileOutputStream=new FileOutputStream("/Users/gemi/Desktop/3.txt");
+        FileChannel fileChannel2=fileOutputStream.getChannel();
+
+        //transferFrom 复制通道信息
+        fileChannel2.transferFrom(fileChannel,0,fileChannel.size());
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
