d9d7cb06904930cd1aea3e8f93daadb6db7418fd
G-keith <1605763261@qq.com>
2021-01-14 14:39:24 +0800

    buffer案例

--- src/main/java/com/keith/nio/TestBuffer.java
+++ src/main/java/com/keith/nio/TestBuffer.java
@@ -12,38 +12,39 @@ import java.nio.IntBuffer;
 public class TestBuffer {
     public static void main(String[] args) {
 
+        //创建一个buffer，大小为5
         IntBuffer intBuffer = IntBuffer.allocate(5);
 
         //向 buffer 存放数据
-        intBuffer.put(10);
-        intBuffer.put(11);
-        intBuffer.put(12);
-        intBuffer.put(13);
-        intBuffer.put(14);
+        for (int i = 0; i < intBuffer.capacity(); i++) {
+            intBuffer.put(i * 2);
+        }
         //position=5
+        read(intBuffer);
+        readOnly(intBuffer);
+    }
 
+    public static void read(IntBuffer intBuffer) {
         //如何从 buffer 读取数据
         // 将 buffer 转换，读写切换(!!!)
         //flip  将position置为0，从头开始读
         intBuffer.flip();
-        //read(intBuffer);
-        readOnly(intBuffer);
-    }
-
-    public static void read(IntBuffer intBuffer){
         //判断是否还有元素，position=limit
         while (intBuffer.hasRemaining()) {
-            System.out.println(intBuffer.get());
+            System.out.print(intBuffer.get()+" ");
         }
+        System.out.println();
     }
 
-    public static void readOnly(IntBuffer intBuffer){
+    public static void readOnly(IntBuffer intBuffer) {
+        intBuffer.flip();
         //只读buffer
-        IntBuffer readOnlyBuffer=intBuffer.asReadOnlyBuffer();
+        IntBuffer readOnlyBuffer = intBuffer.asReadOnlyBuffer();
         //判断是否还有元素，position=limit
         while (intBuffer.hasRemaining()) {
-            System.out.println(intBuffer.get());
+            System.out.print(intBuffer.get()+" ");
         }
-        readOnlyBuffer.put(11);
+        System.out.println();
+        //readOnlyBuffer.put(11);
     }
 }
