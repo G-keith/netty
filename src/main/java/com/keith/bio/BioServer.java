dbc88b63eaa678e9ada56232dccd9709b31e9224
G-keith <1605763261@qq.com>
2021-01-14 13:09:00 +0800

    bio 服务器

--- src/main/java/com/keith/bio/BioServer.java
+++ src/main/java/com/keith/bio/BioServer.java
@@ -11,6 +11,9 @@ import java.util.concurrent.*;
 
 /**
  * bio服务器端
+ * 1.服务器端启动一个ServerSocket
+ * 2.客户端启动一个Socket对服务器进行通讯，默认情况下服务器端需要对每个客户建立一个线程与之通讯
+ * 3.客户端发出请求之后，下咨询服务器是否有线程响应，如果没有则会等待或者被拒绝；如果响应，客户端线程会等待请求结束后才继续执行
  *
  * @author keith
  * @version 1.0
@@ -28,6 +31,8 @@ public class BioServer {
         //创建ServerSocket
         ServerSocket serverSocket = new ServerSocket(8808);
         log.debug("服务端启动成功");
+
+        //noinspection InfiniteLoopStatement
         while (true) {
             log.debug(" 线 程 信 息 id =" + Thread.currentThread().getId() + " 名 字 =" + Thread.currentThread().getName());
             log.debug("等待连接");
@@ -72,6 +77,12 @@ public class BioServer {
         }
     }
 
+    /**
+     * 1.创建一个线程池
+     * 2.如果有客户端连接，就创建一个线程，与之通讯
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         BioServer bioServer = new BioServer();
         bioServer.startServer();
