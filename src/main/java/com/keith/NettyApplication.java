2781dc2aab2666d6b917c8c57e03b2ca2cece3fd
G-keith <1605763261@qq.com>
2021-02-06 16:29:05 +0800

    服务启动自动启动nettyServer

--- src/main/java/com/keith/NettyApplication.java
+++ src/main/java/com/keith/NettyApplication.java
@@ -1,13 +1,18 @@
 package com.keith;
 
+import com.keith.demo.netty.GatewayServer;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.keith.demo.mapper"})
 public class NettyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NettyApplication.class, args);
+        GatewayServer gatewayServer = new GatewayServer(9002);
+        gatewayServer.run();
     }
 
 }
